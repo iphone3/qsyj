765c37c48be47d852620ca00697623b00083ef9a
Atom <313770142@qq.com>
2019-05-24 11:47:12 +0800

    购物车之全选和总计处理

--- src/components/CartItemView/CartItemView.vue
+++ src/components/CartItemView/CartItemView.vue
@@ -1,7 +1,7 @@
 <template>
 	<div class="cart-item-view">
 		<div class="cart-checkbox">
-			<van-checkbox v-model="checked" checked-color="red" class='chekbox'></van-checkbox>
+			<van-checkbox v-model="checked" checked-color="red" class='chekbox' @change='onCheckedChange'></van-checkbox>
 			<span></span>
 		</div>
 		
@@ -26,7 +26,7 @@
 	
 	export default{
 		name: 'cartItemView',
-		props: ['cart'],
+		props: ['cart', 'isChecked', 'index'],
 		components:{
 			[Checkbox.name]:Checkbox
 		},
@@ -34,10 +34,84 @@
 			return {
 			  checked: true
 			};
-		  }
+		},
+		created() {
+			this.checked = this.isChecked
+		},
+		methods:{
+			onCheckedChange: function(){
+				// console.log(this.checked)
+				this.$emit('onChangeChecked', this.checked, this.index)
+			},
+			onItemViewChange: function(checked){
+				console.log('父组件触发')
+				this.checked = checked
+			}
+		}
 	}
 </script>
 
 <style>
-	@import url("../../temp.css");
+.cart-item-view{
+	width: 22.1875rem;
+	height: 6.25rem;
+	margin: 0.625rem;
+	border: 0.0625rem solid #BBBBBB;
+	box-shadow: 0 0 4px 0 gray;
+	border-radius: 0.5rem;
+	background: white;
+}
+
+.cart-item-view > .cart-checkbox{
+	float: left;
+	width: 1.5625rem;
+	height: 100%;
+	text-align: right;
+}
+.cart-item-view > .cart-checkbox > .chekbox{
+	display: inline-block;
+	vertical-align: middle;
+}
+.cart-item-view > .cart-checkbox > span{
+	display: inline-block;
+	vertical-align: middle;
+	height: 100%;
+	width: 0;
+}
+.cart-item-view > .cart-img{
+	width: 4.8125rem;
+	height: 100%;
+	text-align: center;
+	float: left;
+}
+.cart-item-view > .cart-img > img{
+	width: 4.375rem;
+	display: inline-block;
+	vertical-align: middle;
+}
+.cart-item-view > .cart-img > span{
+	display: inline-block;
+	vertical-align: middle;
+	height: 100%;
+	width: 0;
+}
+
+.cart-item-view > .cart-detail{
+	float: left;
+}
+
+.cart-item-view > .cart-detail > .goods-detail{
+	margin-top: 1.5625rem;
+	font-size: 0.875rem;
+}
+.cart-item-view > .cart-detail > .goods-detail > span:nth-of-type(1){
+	color: red;
+	margin-right: 0.6875rem;
+}
+.cart-item-view > .cart-detail > p{
+	font-size: 0.625rem;
+}
+.cart-item-view > .cart-detail  > p:nth-of-type(1){
+	line-height: 2.1875rem;
+}
 </style>
--- src/components/CartView/CartView.vue
+++ src/components/CartView/CartView.vue
@@ -1,23 +1,59 @@
 <template>
 	<div class="cart-view">
-		<CartItemView v-for="cart in carts" :key="cart.id" :cart='cart'>
+		<CartItemView v-for="(cart, index) in carts" :key="cart.id" :cart='cart' :isChecked='selectList[index]' @onChangeChecked='onItemCheckedChange' :index='index' ref='cartItemView'> 
 		</CartItemView>
+		
+		
+		<van-submit-bar
+		  :price="totalPrice"
+		  button-text="提交订单"
+		  @submit="onSubmit"
+		>
+		  <van-checkbox v-model="isAll" class='all-checkbox' checked-color="red" @change='onAllSelect'>全选</van-checkbox>
+		</van-submit-bar>
 	</div>
 </template>
 
 <script>
 	import CartItemView from '../CartItemView/CartItemView.vue'
+	import { SubmitBar,Checkbox } from 'vant';
 	
 	export default{
 		name:'cartView',
 		components:{
-				CartItemView
+				CartItemView,
+				[SubmitBar.name]:SubmitBar,
+				[Checkbox.name]:Checkbox
 		},
 		data() {
 			return {
 				title: '购物车',
 				navViewShow: true,
-				carts: null
+				carts: null,
+				isAll: false,
+				selectList: []
+			}
+		},
+		methods:{
+			onSubmit: function(){
+				console.log('提交订单')
+			},
+			onItemCheckedChange: function(isChecked, index){
+				// console.log(isChecked, index)
+				// 更新状态数组
+				// this.selectList[index] = isChecked
+				// console.log(this.selectList)
+				
+				this.$set(this.selectList, index, isChecked)
+			},
+			onAllSelect: function(){
+				for(var i=0; i<this.selectList.length; i++){
+					// this.selectList[i] = this.isAll
+					this.$set(this.selectList, i, this.isAll)
+					
+					// 需要将对应的状态传递到子组件
+					this.$refs.cartItemView[i].onItemViewChange(this.isAll)
+				}
 			}
 		},
 		created() {
@@ -30,16 +66,35 @@
 				.then(function(response){
 					// console.log(response.data)
 					that.carts = response.data
+					
+					// 动态添加
+					for(var i=0; i<that.carts.length; i++){
+						that.selectList.push(true)
+					}
 				})
 				.catch(function(err){
 					console.log(err)
 				})
+		},
+		computed:{
+			totalPrice: function(){
+				var total = 0
+				
+				for(var i=0; i<this.selectList.length; i++){
+					if(this.selectList[i]){	// 选中
+						var price = this.carts[i].price
+						var num = this.carts[i].num
+						total += price * num
+					}
+				}
+				
+				// 注意显示单位
+				return total * 10 * 10
+			}
 		}
 	}
 </script>
 
 <style>
-	.cart-view{
-		padding-top: 2.8125rem;
-	}
+	@import url("../../temp.css");
 </style>
