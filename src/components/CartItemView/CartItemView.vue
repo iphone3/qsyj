a35d2149c39b110062947b2b942912d94d0b6253
Atom <313770142@qq.com>
2019-05-24 10:14:46 +0800

    购物车之商品显示

---
+++ src/components/CartItemView/CartItemView.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="cart-item-view">
+		<div class="cart-checkbox">
+			<van-checkbox v-model="checked" checked-color="red" class='chekbox'></van-checkbox>
+			<span></span>
+		</div>
+		
+		<div class="cart-img">
+			<img :src="cart.img" alt="">
+			<span></span>
+		</div>
+		
+		<div class="cart-detail">
+			<div class="goods-detail">
+				<span>特价</span>
+				<span>{{cart.name}}</span>
+			</div>
+			<p>￥{{cart.price}}</p>
+			<p>x{{cart.num}}</p>
+		</div>
+	</div>
+</template>
+
+<script>
+	import { Checkbox } from 'vant';
+	
+	export default{
+		name: 'cartItemView',
+		props: ['cart'],
+		components:{
+			[Checkbox.name]:Checkbox
+		},
+		data() {
+			return {
+			  checked: true
+			};
+		  }
+	}
+</script>
+
+<style>
+	@import url("../../temp.css");
+</style>
--- src/components/CartView/CartView.vue
+++ src/components/CartView/CartView.vue
@@ -1,24 +1,45 @@
 <template>
 	<div class="cart-view">
-		购物车
+		<CartItemView v-for="cart in carts" :key="cart.id" :cart='cart'>
+		</CartItemView>
 	</div>
 </template>
 
 <script>
+	import CartItemView from '../CartItemView/CartItemView.vue'
+	
 	export default{
 		name:'cartView',
+		components:{
+				CartItemView
+		},
 		data() {
 			return {
 				title: '购物车',
-				navViewShow: true
+				navViewShow: true,
+				carts: null
 			}
 		},
 		created() {
 			this.$emit('onTitle', this.title)
 			this.$emit('onNavShow', this.navViewShow)
+			
+			var that = this
+			
+			this.$ajax.get('/json/cart.json')
+				.then(function(response){
+					// console.log(response.data)
+					that.carts = response.data
+				})
+				.catch(function(err){
+					console.log(err)
+				})
 		}
 	}
 </script>
 
 <style>
+	.cart-view{
+		padding-top: 2.8125rem;
+	}
 </style>
